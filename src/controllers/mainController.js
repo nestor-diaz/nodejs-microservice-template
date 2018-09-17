572e627f0fee1541f8efe413a6fb551b5e712d20
Nestor Diaz <nestor.loaiza@mulesoft.com>
2018-09-16 19:29:29 -0500

    - Implement post, put and delete handlers

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,12 +1,33 @@
 function mainController({ mainService }) {
   return {
     get,
+    post,
+    put,
+    deleteFn,
   };
 
   async function get(req, res) {
     const getResponse = await mainService.get();
 
     return res.json(getResponse);
+  }
+
+  async function post(req, res) {
+    const postResponse = await mainService.get();
+
+    return res.json(postResponse);
+  }
+
+  async function put(req, res) {
+    const putResponse = await mainService.get();
+
+    return res.json(putResponse);
+  }
+
+  async function deleteFn(req, res) {
+    const deleteResponse = await mainService.get();
+
+    return res.json(deleteResponse);
   }
 }
 
--- src/services/mainService.js
+++ src/services/mainService.js
@@ -1,15 +1,29 @@
 function mainService() {
   return {
-    get,
-  };
-
-  function get() {
-    return new Promise((resolve) => {
+    get: () => new Promise((resolve) => {
       setTimeout(() => {
-        resolve({ GET: 'Main Controller / Service response' });
+        resolve({ GET: 'Main Service / GET response' });
       }, 2000);
-    });
-  }
+    }),
+
+    post: () => new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ GET: 'Main Service / POST response' });
+      }, 2000);
+    }),
+
+    put: () => new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ GET: 'Main Service / PUT response' });
+      }, 2000);
+    }),
+
+    delete: () => new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ GET: 'Main Service / DELETE response' });
+      }, 2000);
+    }),
+  };
 }
 
 module.exports = mainService;
